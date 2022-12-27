2de2e5ca8824f9be8f71b453c5421c4147409f24
金井伸也 <kanaishinya@skanaiMBA.local>
2022-12-27 19:16:26 +0900

    add feature upload and analyze

--- vue.js
+++ vue.js
@@ -1,9 +1,20 @@
 var debug=true;
 var debug=false;
+var env="standalone";
+// var env="gas";
 
 const minNumPlayer = 4;
 const maxNumPlayer = 10;
 const maxToken = 20;
+const defaultState = {
+    numPlayer:minNumPlayer,
+    playerList:[],
+    auctionItems:[],
+    currentState:0,
+    auctionAssginment:[],
+    auctionLog:[],
+    debug:debug
+};
 
 /**
  * 一様分布に従う乱数を生成する関数
@@ -79,22 +90,34 @@ const GameSetting = {
     data() {
         return {
             fixedNumPlayer:false,
-            notSameName:[]
+            notSameName:[],
+            fixedClassId:this.gameInfo.classId,
+            fixedGroupId:this.gameInfo.groupId,
+            fixedSessionId:this.gameInfo.sessionId
         }
     },
     mounted() {
         
     },
     computed:{
-        notValidNumPlayer() {
-            return (this.numPlayer < minNumPlayer)|(maxNumPlayer < this.numPlayer)
+        validNumPlayer() {
+            return (this.numPlayer >= minNumPlayer)&(maxNumPlayer >= this.numPlayer)
         },
+        fillRequirments(){
+            return Boolean(this.fixedClassId) & Boolean(this.fixedGroupId) & Boolean(this.fixedSessionId)
+        },
+        canProceed(){
+            return this.validNumPlayer & this.fillRequirments
+        }
         // notSameName(){
         //     // return this.tempPlayerNames.map((playerName,id) => playerName != this.playerList[id])
         //     return this.tempPlayerNames
         // }
     },
     methods:{
+        setupGameInfo() {
+            this.setGameInfo(this.fixedClassId,this.fixedGroupId,this.fixedSessionId)
+        },
         checkData() {
             console.log(this)
         },
@@ -128,28 +151,46 @@ const GameSetting = {
             // console.log(this.tempPlayerNames)
         }
     },
-    props: ["numPlayer","setNumPlayer","playerList","initPlayerList","setPlayerName","tempPlayerNames","currentState","incrementCurrentState","decrementCurrentState","generateAuctionItems","assignPlayer2AuctionItem","initAuctionLog"],
+    props: ["gameInfo","numPlayer","setNumPlayer","playerList","initPlayerList","setPlayerName","tempPlayerNames","currentState","incrementCurrentState","decrementCurrentState","generateAuctionItems","assignPlayer2AuctionItem","initAuctionLog","setGameInfo"],
     emits: ["update:numPlayer","update:currentState"],
     template: /*html*/`
     <div id="num-player-setting"
     v-if="currentState == 0">
-        <h1>何人で遊びますか？</h1>
+        <h2>基本情報を入力してください</h2>
         <div class="row">
             <div class="col">
-            <div class="input-group">
-                <button type="button" class="btn btn-secondary  pe-3 ps-3"
-                @click="$emit('update:numPlayer', numPlayer-1)"> - </button>
-                <input type="number" id="num-player" class="form-control"
-                    :value="numPlayer"
-                    @input="$emit('update:numPlayer', $event.target.value)">    
-                    <button type="button" class="btn btn-secondary  pe-3 ps-3"
-                    @click="$emit('update:numPlayer', numPlayer+1)"> + </button>    
-            </div>
+                <label for="class-id">クラス名</label>
+                <input id="class-id" class="form-control"
+                v-model="fixedClassId">
             </div>
             <div class="col">
+                <label for="group-id">グループ名</label>
+                <input id="group-id" class="form-control"
+                v-model="fixedGroupId">
+            </div>
+            <div class="col">
+                <label for="session-id">ゲーム回数</label>
+                <input id="session-id" class="form-control" 
+                v-model="fixedSessionId">
+            </div>
+            <div class="col">
+                <label for="num-player">プレイヤー人数</label>
+                <div class="input-group">
+                    <button type="button" class="btn btn-secondary  pe-3 ps-3"
+                    @click="$emit('update:numPlayer', numPlayer-1)"> - </button>
+                    <input type="number" id="num-player" class="form-control"
+                        :value="numPlayer"
+                        @input="$emit('update:numPlayer', $event.target.value)">    
+                        <button type="button" class="btn btn-secondary  pe-3 ps-3"
+                        @click="$emit('update:numPlayer', numPlayer+1)"> + </button>    
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
                 <button type="button" class="button btn btn-primary"
-                :class="{'disabled': notValidNumPlayer}"
-                @click="initTempPlayerList();incrementCurrentState()">次へ</button>
+                :class="{'disabled': !canProceed}"
+                @click="setupGameInfo();initTempPlayerList();incrementCurrentState()">次へ</button>
             </div>
         </div>
     </div>
@@ -172,7 +213,7 @@ const GameSetting = {
                         <button type="button" class="button btn btn-secondary"
                         @click="decrementCurrentState">戻る</button>                        
                         <button type="button" class="button btn btn-primary"
-                        :class="{'disabled': notValidNumPlayer}"
+                        :class="{'disabled': !canProceed}"
                         @click="endSetting();">次へ</button>
                     </div>
                 </div>
@@ -434,8 +475,188 @@ const GameMain = {
                 </ul>
             </div>
         </div>
-
     </div>
+    `
+}
+
+
+/**
+ * 可視化のVueモデル
+ */
+const Analyzer = {
+    data() {
+        return {
+            first_x: '<b>開始</b>',
+            last_x: '<b>終了</b>',
+            first_y: 0,
+            color_setting: {
+                bid: 'royalblue',
+                sell: 'firebrick',
+                buy: 'darkorchid',
+                appr: 'mediumseagreen'
+            },
+            x_base:[],
+            y_base:[],
+            tagId:"item"+this.itemId
+        }
+    },
+    computed:{
+        last_y(){
+            return this.y_base[this.y_base.length-1]
+        },
+        appr_min() {
+            return this.playData.auctionItems[this.itemId].interval.min
+        },
+        appr_max() {
+            return this.playData.auctionItems[this.itemId].interval.max
+        },
+        buy_price() {
+            return this.playData.auctionItems[this.itemId].buy
+        },
+        sell_price() {
+            return this.playData.auctionItems[this.itemId].sell
+        },
+        buyer_name() {
+            return this.playData.playerList[this.playData.auctionAssginment[this.itemId].sell].name
+        },
+        appr_name() {
+            return this.playData.playerList[this.playData.auctionAssginment[this.itemId].appr].name
+        },
+        bidder_name() {
+            return this.playData.playerList[this.playData.auctionLog[this.itemId].buyer].name
+        },
+        item_name() {
+            return '絵画'+this.itemId
+        },
+        trace_data() {
+            var log = {
+                x: [this.first_x,this.x_base,this.last_x].flat(),
+                y: [this.first_y,this.y_base,this.last_y].flat(),
+                mode: 'lines+markers',
+                name: '入札額',
+                line:{shape: 'hv',color: this.color_setting.bid}
+            };
+            var range = {
+                x: [this.first_x,this.x_base,this.last_x,this.last_x,this.x_base,this.first_x,this.first_x].flat(),
+                y: [Array(this.x_base.length+2).fill(this.appr_max),Array(this.x_base.length+2).fill(this.appr_min),this.appr_max].flat(),
+                name: '鑑定額(上限)',
+                fill: 'tozerox',
+                line: {color: this.color_setting.appr}
+            };
+            var range_min = {
+                x: [this.first_x,this.x_base,this.last_x].flat(),
+                y: Array(this.x_base.length+2).fill(this.appr_min),
+                name: '鑑定額(下限)',
+                mode: 'lines',
+                fill: 'tozerox',
+                line: {color: this.color_setting.appr}
+            };
+            var selling = {
+                x: [this.first_x,this.x_base,this.last_x].flat(),
+                y: Array(this.x_base.length+2).fill(this.sell_price),
+                name: '売却額',
+                mode: 'lines',
+                line: {
+                    dash: 'dash',
+                    color: this.color_setting.sell
+                }
+            }
+            var buying = {
+                x: [this.first_x,this.x_base,this.last_x].flat(),
+                y: Array(this.x_base.length+2).fill(this.buy_price),
+                name: '仕入れ額',
+                mode: 'lines',
+                line: {
+                    dash: 'dot',
+                    color: this.color_setting.buy
+                }
+            };
+            
+            return [range,range_min,selling,buying,log]
+        },
+        options() {
+            // var buyer = {
+            //     x: this.first_x,
+            //     y: this.buy_price,
+            //     xanchor: 'right',
+            //     yanchor: 'middle',
+            //     showarrow: false,
+            //     font: {
+            //         family: 'Arial',
+            //         size: 16,
+            //         color: 'black'
+            //       },
+            //     text: '仕入れ<br>('+this.buyer_name+')'
+            // };
+            // var appraisal = {
+            //     x: this.first_x,
+            //     y: this.appr_max,
+            //     xanchor: 'right',
+            //     yanchor: 'middle',
+            //     showarrow: false,
+            //     font: {
+            //         family: 'Arial',
+            //         size: 16,
+            //         color: 'black'
+            //       },
+            //     text: '鑑定<br>('+this.appr_name+')'
+            // };
+            var bidder = {
+                x: this.last_x,
+                y: this.last_y,
+                xanchor: 'left',
+                yanchor: 'middle',
+                showarrow: false,
+                font: {
+                    family: 'Arial',
+                    size: 16,
+                    color: 'black'
+                  },
+                text: '<span style="color:'+this.color_setting.bid+';"> '+this.bidder_name+' </span>'
+            };
+            return {
+                title: this.item_name,
+                annotations:[bidder]
+            }
+        }
+    },
+    methods:{
+    },
+    mounted: function() {
+        console.log("analyzing");
+        var players = [];
+        var turn_num = 1;
+        this.playData.auctionLog[this.itemId].history.map((turn,id) => {
+            if (players.includes(turn.playerId)){
+                turn_num ++;
+                players = [];
+            }
+            players.push(turn.playerId);
+            var player_color = '';
+            if (turn.playerId == this.playData.auctionAssginment[this.itemId].sell){
+                player_color = this.color_setting.buy
+            } else if (turn.playerId == this.playData.auctionAssginment[this.itemId].appr) {
+                player_color = this.color_setting.appr
+            } else if (turn.playerId == this.playData.auctionLog[this.itemId].buyer){
+
+            }
+            var player_name = '<b style="color: '+player_color+'">'+this.playData.playerList[turn.playerId].name+'</b>' + '<br>' + turn_num +'回目';
+            if (turn.bid != 0) {
+                this.x_base.push(player_name);
+                this.y_base.push(turn.bid);
+            } else {
+                this.x_base.push(player_name);
+                this.y_base.push(turn.bid);
+                this.x_base.push(player_name);
+                this.y_base.push(this.y_base[this.y_base.length-2])
+            }
+        });
+        Plotly.plot(this.tagId, this.trace_data, this.options);
+    },
+    props:['playData','itemId'],
+    components:{},
+    template:/*html*/`
+    <div :id="tagId"></div>
     `
 }
 
@@ -445,16 +666,29 @@ const GameMain = {
 const GamePayment = {
     data() {
         return {
-            sessionUrl:null
+            sessionUrl:null,
+            uploaded:false,
+            view:'table'
         }
     },
-    props:["playerList","auctionAssginment","auctionItems","auctionLog","currentState","incrementCurrentState","decrementCurrentState","checkPayment","checkResult"],
+    props:["gameInfo","playerList","auctionAssginment","auctionItems","auctionLog","currentState","incrementCurrentState","decrementCurrentState","checkPayment","checkResult","uploadPlayData","env"],
     computed:{
         fileName() {
             return "playdata.json" // 時刻とか何らかの識別子入れる
         },
+        playData() {
+            return {
+                gameInfo:this.gameInfo,
+                playerList:this.playerList,
+                auctionAssginment:this.auctionAssginment,
+                auctionItems:this.auctionItems,
+                auctionLog:this.auctionLog,
+                result:this.checkResult()
+            }
+        },
         jsonData() {
             jsonData = JSON.stringify({
+                gameInfo:this.gameInfo,
                 playerList:this.playerList,
                 auctionAssginment:this.auctionAssginment,
                 auctionItems:this.auctionItems,
@@ -467,6 +701,7 @@ const GamePayment = {
     methods:{
         jasonify() {
             jsonData = {
+                gameInfo:this.gameInfo,
                 playerList:this.playerList,
                 auctionAssginment:this.auctionAssginment,
                 auctionItems:this.auctionItems,
@@ -478,12 +713,25 @@ const GamePayment = {
         createDownloadUrl() {
             blob = new Blob([this.jsonData], { type: 'application/json' })
             this.sessionUrl = window.URL.createObjectURL(blob)
+        },
+        upload(){
+            this.uploadPlayData(this.jsonData)
+            this.uploaded = true
+        },
+        toggle(){
+            if (this.view == 'table') {
+                this.view = 'plot'
+            } else {
+                this.view = 'table'
+            }
+            console.log(this.view)
         }
     },
+    components:{'analyzer':Analyzer},
     template:/*html*/`
     <div id="payment"
     v-if="currentState == 4">
-        <table class="table">
+        <table class="table" v-if="view == 'table'">
             <thead>
                 <tr>
                     <th></th>
@@ -525,8 +773,15 @@ const GamePayment = {
                 </tr>
             </tbody>
         </table>
+        <div v-if="view == 'plot'">
+            <analyzer v-for="(item,index) in playData.auctionItems" :key="item" :play-data="playData" :item-id="index"></analyzer>
+        </div>
         <a type="button" class="btn btn-info mt-3" id="dataDL"
         :href="sessionUrl" :download="fileName" @click="createDownloadUrl">プレイデータをDL</a>
+        <a type="button" class="btn btn-info mt-3" id="dataUL"
+        v-if="env=='gas'" @click="upload" :class="{'disabled': uploaded}">プレイデータをUL</a>
+        <button type="button" class="btn btn-info mt-3" id="dataAnalyze"
+        @click="toggle">表示を切り替え</button>
     </div>
     `
 }
@@ -537,13 +792,19 @@ const GamePayment = {
 const Game = {
     data() {
         return {
+            gameInfo:{
+                classId:"",
+                groupId:"",
+                sessionId:""
+            },
             numPlayer:minNumPlayer,
             playerList:[],
             auctionItems:[],
             currentState:0,
             auctionAssginment:[],
             auctionLog:[],
-            debug:debug
+            debug:debug,
+            env:env
         }
     },
     computed:{
@@ -552,6 +813,11 @@ const Game = {
         }
     },
     methods:{
+        setGameInfo(classId,groupId,sessionId){
+            this.gameInfo.classId = classId
+            this.gameInfo.groupId = groupId
+            this.gameInfo.sessionId = sessionId
+        },
         checkData() {
             console.log(this)
         },
@@ -663,7 +929,14 @@ const Game = {
             this.currentState=0
             this.auctionAssginment=[]
             this.auctionLog=[]
-        }
+        },
+        uploadPlayData(json) {
+            google.script.run.withSuccessHandler(this.successMessage).submit(json)
+        },
+        successMessage(response) {
+            alert('送信が完了しました\n')
+            console.log(JSON.parse(response))
+        },
     },
     components:{'game-setting': GameSetting,"card-check": CardCheck,"game-main":GameMain,"game-payment":GamePayment},
     template:/*html*/`
@@ -674,6 +947,7 @@ const Game = {
     v-model:num-player="numPlayer"
     v-model:player-list="playerList"
     v-model:set-player-name="setPlayerName"
+    :game-info="gameInfo"
     :set-num-player="setNumPlayer"
     :init-player-list="initPlayerList"
     :temp-player-names="playerNames"
@@ -682,7 +956,8 @@ const Game = {
     :decrement-current-state="decrementCurrentState"
     :generate-auction-items="generateAuctionItems"
     :assign-player2-auction-item="assignPlayer2AuctionItem"
-    :init-auction-log="initAuctionLog"></game-setting>
+    :init-auction-log="initAuctionLog"
+    :set-game-info="setGameInfo"></game-setting>
 
     <card-check
     :player-list="playerList"
@@ -706,6 +981,7 @@ const Game = {
     :upload-result="uploadResult"></game-main>
 
     <game-payment
+    :game-info="gameInfo"
     :player-list="playerList"
     :auction-assginment="auctionAssginment"
     :auction-items="auctionItems"
@@ -714,7 +990,9 @@ const Game = {
     :decrement-current-state="decrementCurrentState"
     :auction-log="auctionLog"
     :check-payment="checkPayment"
-    :check-result="checkResult"></game-payment>
+    :check-result="checkResult"
+    :upload-play-data="uploadPlayData"
+    :env="env"></game-payment>
 
     <div
     v-if="debug">
